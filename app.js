9c547ca09454dddab2f64bb1e9e984fc70d9e4dc
Akinyemi Oluwayemisi <Yemi@MACs-MacBook-Pro.local>
2022-03-24 01:08:11 +0100

    update

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ window.addEventListener('scroll', (e) => {
     const header = document.querySelector('header');
     const headerHeight = header.offsetHeight
     const scrollHeight = window.scrollY
+    console.log(scrollHeight)
     if ( scrollHeight <= '1') {
         header.classList.remove('scrolled');
     } else{ 
@@ -19,3 +20,15 @@ window.addEventListener('scroll', (e) => {
     }
 })
 
+
+ScrollReveal().reveal('.security-section', {delay:100});
+ScrollReveal().reveal('.savings-section', {delay:100});
+ScrollReveal().reveal('.piggybank', {delay:200});
+ScrollReveal().reveal('.safelock', {delay:200});
+ScrollReveal().reveal('.targetSavings', {delay:200});
+ScrollReveal().reveal('.flexNaira', {delay:200});
+ScrollReveal().reveal('.invest-section', {delay:100});
+ScrollReveal().reveal('.SOTM-section', {delay:100});
+ScrollReveal().reveal('.customers', {delay:100});
+ScrollReveal().reveal('.features-section', {delay:100});
+ScrollReveal().reveal('.footer-section', {delay:100});
